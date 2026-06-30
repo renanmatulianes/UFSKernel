3bb95a0e9debe324a3d563d16ec34e84acfa5ac6
renanmatulianes <renanmatulianes@estudante.ufscar.br>
2026-06-29 21:27:22 -0300

    Revert "não sobrescrever um handler"

--- kernel/main.c
+++ kernel/main.c
@@ -1,27 +1,10 @@
-#include <interrupts_handler.h>
 #include <serial.h>
-#include <timer.h>
 
+// O linker manda o contador de programa para o binário daqui.
 void kmain(void) {
-  serial_init();
-  serial_puts("Bem-vindo ao UFSKernel!\n");
-  serial_puts("Executando em modo ARM bare-metal no QEMU.\n");
+    serial_init();
+    serial_puts("Bem-vindo ao UFSKernel!\n");
+    serial_puts("Executando em modo ARM bare-metal no QEMU.\n");
 
-  serial_puts("Preparando CPU (VBAR e Pilha IRQ)...\n");
-  setup_core_for_irq();
-
-  serial_puts("Configurando GIC...\n");
-  init_gic();
-
-  serial_puts("Ligando interrupções...\n");
-  enable_cpu_interrupts();
-
-  serial_puts("Ativando Timer...\n");
-  init_timer();
-
-  while (1) {
-    serial_puts("Kernel rodando na main...\n");
-    // Um delay apenas para não flodar a tela
-    for (volatile int i = 0; i < 50000000; i++);
-  }
+    return;
 }
